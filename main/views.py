51bb30d15e662395d3b76204a0ab1b13d5295e18
VedanshKumarKawadkar <67778396+VedanshKumarKawadkar@users.noreply.github.com>
2022-01-29 13:14:39 +0530

    day 2 commit 1

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+import pymongo
 # Create your views here.
 
 def mainpage(request):
     return render(request, "mainpage.html")
+
+def add_visitor():
+    ...
